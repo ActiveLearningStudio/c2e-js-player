4ade73c59c1180b80acd5b5e5459ee364169a7ff
aqibnwaz <aqib@curriki.org>
2023-01-19 17:56:45 +0500

    offline c2e project

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AddC2e from "./c2e/addc2e";
 import Myc2e from "./c2e/mysc2e";
-import Test from "./c2e/test";
+
 import "./App.css";
 
 function App() {
@@ -15,9 +15,7 @@ function App() {
         <Route exact path="/myC2e">
           <Myc2e />
         </Route>
-        <Route exact path="/test">
-          <Test />
-        </Route>
+        
       </Switch>
     </Router>
   );
--- src/c2e/addc2e.tsx
+++ src/c2e/addc2e.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import JSZip from 'jszip';
-import H5PEditor from '../components/H5PEditor';
+import React, { useEffect, useState } from "react";
+import JSZip from "jszip";
+import H5PEditor from "../components/H5PEditor";
+import { Link } from "react-router-dom";
 
 function AddC2e() {
   const [contentData, setcontentData] = useState<any>();
@@ -10,20 +11,19 @@ function AddC2e() {
 
   const returnIndex = (filename: any) => {
     return contentData.indexOf(
-      contentData.filter((data: any, counter: any) =>
-        data.includes(filename)
-      )?.[0]
+      contentData.filter((data: any, counter: any) => data.includes(filename))?.[0]
     );
   };
   return (
     <div className="App">
+      <h2><Link to="myC2e">Offline C2E file</Link></h2>
       <h2>Upload C2E file</h2>
-      
+
       <div className="">
         <input
           type="file"
           onChange={async (e: any) => {
-            console.log('name', e.target.files);
+            console.log("name", e.target.files);
             const loadzip = await JSZip.loadAsync(e.target.files[0]); // 1) read the Blob
             console.log(loadzip);
             const contents: any = [];
@@ -33,11 +33,9 @@ function AddC2e() {
             });
             setcontentData(contents);
             for (var i = 0; i < contents.length; i++) {
-              const contentRead = await loadzip.files[contents[i]].async(
-                'text'
-              );
+              const contentRead = await loadzip.files[contents[i]].async("text");
               contentsDetail.push(contentRead);
-              if (contents[i].includes('project.json')) {
+              if (contents[i].includes("project.json")) {
                 setProjectJSON(JSON.parse(contentRead));
               }
             }
@@ -59,8 +57,7 @@ function AddC2e() {
       {contentData && (
         <>
           <p>
-            There are <strong>{contentData?.length}</strong> files found in
-            given zip
+            There are <strong>{contentData?.length}</strong> files found in given zip
           </p>
           {/* <div className="contentScroll">
             {contentDetail?.map((data: any, counter: number) => {
@@ -76,45 +73,42 @@ function AddC2e() {
       )}
       <div
         style={{
-          display: 'flex',
-          gap: '10px',
-          marginBottom: '10px',
-
+          display: "flex",
+          gap: "10px",
+          marginBottom: "10px",
         }}
       >
         {projectJSON?.playlists.map((playlist: any) => {
           return (
-            <div key={playlist.id} className="playlist-wrapper"  style={{
-              background:'#fff',
-              border: '1px solid #ccc',
-              padding: '15px',
-            }}>
+            <div
+              key={playlist.id}
+              className="playlist-wrapper"
+              style={{
+                background: "#fff",
+                border: "1px solid #ccc",
+                padding: "15px",
+              }}
+            >
               <h4>{playlist.title}</h4>
-              <div className="activitylist-wrapper"  >
+              <div className="activitylist-wrapper">
                 {playlist?.activities.map((activity: any) => {
                   return (
                     <div
                       style={{
-                        display: 'flex',
-                        gap: '10px',
-                        marginBottom: '10px',
+                        display: "flex",
+                        gap: "10px",
+                        marginBottom: "10px",
                       }}
                       key={activity.id}
                       onClick={() =>
                         setActivityh5p(
                           JSON.parse(
-                            contentDetail[
-                              returnIndex(`${activity.h5p_content_id}-h5p.json`)
-                            ]
+                            contentDetail[returnIndex(`${activity.h5p_content_id}-h5p.json`)]
                           )
                         )
                       }
                     >
-                      <img
-                        style={{ width: '50px' }}
-                        src={activity.thumb_url}
-                        alt=""
-                      />
+                      <img style={{ width: "50px" }} src={activity.thumb_url} alt="" />
                       <p>{activity.title}</p>
                     </div>
                   );
